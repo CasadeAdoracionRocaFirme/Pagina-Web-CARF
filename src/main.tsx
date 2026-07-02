87f78b19d37debc1e123e430c55d8d4216074656
Andres Castro <188876553+elcasti04@users.noreply.github.com>
2026-07-02 13:19:48 -0500

    quitando browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import App from './App.tsx'
 
@@ -9,7 +8,7 @@ createRoot(document.getElementById('root')!).render(
   
     <StrictMode>
       <HashRouter>
-    <App />
+        <App />
       </HashRouter>
   </StrictMode>,
 
