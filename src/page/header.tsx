c9075158ceb7771261ddc0a9b13c94a70d950e4e
Andres Castro <188876553+elcasti04@users.noreply.github.com>
2026-07-02 16:00:07 -0500

    ajustando media query´s

--- src/page/header.tsx
+++ src/page/header.tsx
@@ -1,52 +1,85 @@
-import { useState } from 'react'
-import '../style/header.css'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import "../style/header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const [abierto, setAbierto] = useState(false)
+    const navigate = useNavigate();
+
+    const [abierto, setAbierto] = useState(false);
+    const [menuAbierto, setMenuAbierto] = useState(false);
+
     return (
-        <>
-        <header className='header'>
-            <div className='logo'>
-                <img height={'65px'} src="/logo.png" alt="" />
+        <header className="header">
+
+            <div className="logo">
+                <img height="65" src="/logo.png" alt="Logo" />
             </div>
 
-            <ul className='header-ul'>
-                <li className='list'>Hogar</li>
-                <li className='list'>Sobre Nosotros</li>
-                <li className='ministerios'
-                    onClick={() => setAbierto(!abierto)}
-                    onMouseEnter={() => setAbierto(true)}
-                    onMouseLeave={() => setAbierto(false)}>Ministerios
-                    {abierto && (
-                    <>
-                    <ul className='mins'>
-                        <li onClick={() => navigate('/Damas')} className='list'>Damas</li>
-                        <li onClick={() => navigate('/Caballeros')} className='list'>Caballeros</li>
-                        <li onClick={() => navigate('/Jovenes')} className='list'>Jovenes</li>
-                        <li onClick={() => navigate('/Niños')} className='list'>Niños</li>
-                        <li onClick={() => navigate('/Alabanza')} className='list'>Alabanza</li>
-                        <li onClick={() => navigate('/Consolidación')} className='list'>Consolidación</li>
-                        <li onClick={() => navigate('/Servidores')} className='list'>Servidores</li>
-                        <li onClick={() => navigate('/Oración')} className='list'>Oración</li>
-                        <li onClick={() => navigate('/Protocolo')} className='list'>Protocolo</li>
-                        <li onClick={() => navigate('/Familias')} className='list'>Familias</li>
-                        <li onClick={() => navigate('/Parejas')} className='list'>Parejas</li>
-                        <li onClick={() => navigate('/Danza')} className='list'>Danza</li>
-                        <li onClick={() => navigate('/Casas_de_Oración')} className='list'>Casas_de_Oración</li>
-                    </ul>
-                    </>
-                )}
+            <button
+                className="menu-btn"
+                onClick={() => setMenuAbierto(!menuAbierto)}
+            >
+                ☰
+            </button>
+
+            <ul className={`header-ul ${menuAbierto ? "activo" : ""}`}>
+
+                <li
+                    className="list"
+                    onClick={() => {
+                        navigate("/");
+                        setMenuAbierto(false);
+                    }}
+                >
+                    Hogar
                 </li>
-                <li className='list' onClick={() => navigate('/Contacto')}>Contacto</li>
+
+                <li className="list">
+                    Sobre Nosotros
+                </li>
+
+                <li
+                    className="ministerios"
+                    onClick={() => setAbierto(!abierto)}
+                >
+                    Ministerios
+
+                    {abierto && (
+                        <ul className="mins">
+
+                            <li onClick={() => navigate("/Damas")}>Damas</li>
+                            <li onClick={() => navigate("/Caballeros")}>Caballeros</li>
+                            <li onClick={() => navigate("/Jovenes")}>Jóvenes</li>
+                            <li onClick={() => navigate("/Niños")}>Niños</li>
+                            <li onClick={() => navigate("/Alabanza")}>Alabanza</li>
+                            <li onClick={() => navigate("/Consolidación")}>Consolidación</li>
+                            <li onClick={() => navigate("/Servidores")}>Servidores</li>
+                            <li onClick={() => navigate("/Oración")}>Oración</li>
+                            <li onClick={() => navigate("/Protocolo")}>Protocolo</li>
+                            <li onClick={() => navigate("/Familias")}>Familias</li>
+                            <li onClick={() => navigate("/Parejas")}>Parejas</li>
+                            <li onClick={() => navigate("/Danza")}>Danza</li>
+                            <li onClick={() => navigate("/Casas_de_Oración")}>Casas de Oración</li>
+
+                        </ul>
+                    )}
+
+                </li>
+
+                <li
+                    className="list"
+                    onClick={() => {
+                        navigate("/Contacto");
+                        setMenuAbierto(false);
+                    }}
+                >
+                    Contacto
+                </li>
+
             </ul>
 
-
-
         </header>
-        </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
